39d5b6ed9e76a25f1312192e21c58f9cadaafbe8
Patrick Student <patrick.tumbaga@stu.ocsb.ca>
2025-09-17 17:25:54 +0000

    “basic_math.py”

---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Created By: Patrick
+# Date: 9/16/2025
+# My homework
+
+
+def main():
+    print("7-3={}".format(7 - 3))
+    print("3+2={}".format(3 + 2))
+    print("9*3={}".format(9 * 3))
+    print("4/2={}".format(4 / 2))
+    print("2/4={}".format(2 / 4))
+    print("4+10*6={}".format(4 + 10 * 6))
+    print("4^6={}".format(4 ^ 6))
+    print("6**7={}".format(6**7))
+    print("12**0.5={}".format(12**0.5))
+
+
+if __name__ == "__main__":
+    main()
